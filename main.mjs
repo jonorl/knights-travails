04eb157457be8e081b1819ee8c76d27652a31400
jonorl <39122230+jonorl@users.noreply.github.com>
2024-11-29 16:34:06 +0300

    the knight traverses from starting to end point 6 times... for reasons... Now onto checking the path with the fewest steps.

--- main.mjs
+++ main.mjs
@@ -1,8 +1,8 @@
 // Imports
 
-import { HashMap } from "./class.mjs"
+import { HashMap } from "./class.mjs";
 
-let chessboard = [ 
+let chessboard = [
       // 0  1  2  3  4  5  6  7
   /*0*/ [0, 0, 0, 0, 0, 0, 0, 0],
   /*1*/ [0, 0, 0, 0, 0, 0, 0, 0],
@@ -11,86 +11,119 @@ let chessboard = [
   /*4*/ [0, 0, 0, 0, 0, 0, 0, 0],
   /*5*/ [0, 0, 0, 0, 0, 0, 0, 0],
   /*6*/ [0, 0, 0, 0, 0, 0, 0, 0],
-  /*7*/ [0, 0, 0, 0, 0, 0, 0, 0]]
+  /*7*/ [0, 0, 0, 0, 0, 0, 0, 0],
+];
 
-function knightMoves(arr1, arr2){
-    console.log(arr1)
-//   const knight = new HashMap();
+let queue = [];
+let visitedQueue = new Set([]);
 
-//   console.log(lookForNextMoves(arr1));
-
-  let queue = lookForNextMoves(arr1)
-
-    while (queue.length !== 0) {
-      queue.forEach(element => {
-        if(JSON.stringify(element) !== JSON.stringify(arr2)){
-          console.log(element);
-            }
-        });
-        console.log("finished")
-        if(JSON.stringify(queue[0]) !== JSON.stringify(arr2)){
-            queue.shift();
-            queue = queue.concat(lookForNextMoves(queue[Math.floor(Math.random() * queue.length)]));
-        }
-            else {
-                console.log(queue)
-                return};
-        };
-    queue.shift();
-    console.log(queue)
+function knightMoves(arr1, arr2) {
+  visitedQueue.add(JSON.stringify(arr1));
+  // put the next moves in the queue
+  queue = lookForNextMoves(arr1);
+  // duplicate the queue
+  let nextMoveQueue = queue;
+  // check if the target value is in the queue
+  queue.forEach((move) => {
+    if (JSON.stringify(move) === JSON.stringify(arr2)) {
+      console.log(visitedQueue)
+      return true;
+    }
+  });
+  queue = [];
+  // Compare the nextMoveQueue against the set and remove values if any:
+  nextMoveQueue = nextMoveQueue.filter(
+    (item) => !visitedQueue.has(JSON.stringify(item))
+  );
+  // On the queue clone, look for next moves and put them back in the queue starting all over recursively
+  nextMoveQueue.forEach((move) => {
+    knightMoves(move, arr2);
+  });
 }
 
-//   potentialMove.forEach(element => {
-    // console.log(element)
-//     if(JSON.stringify(element) === JSON.stringify(arr2)){
-        // return the number of steps
-//     }
+function lookForNextMoves(arr1) {
+  let nextPotentialMoveHorizontal = [];
+  let nextPotentialMoveVertical = [];
+  let potentialMove = [];
+  switch (arr1[0]) {
+    case 0:
+      nextPotentialMoveHorizontal.push(1, 2);
+      break;
+    case 1:
+      nextPotentialMoveHorizontal.push(0, 1, 2);
+      break;
+    case 2:
+      nextPotentialMoveHorizontal.push(0, 1, 3, 4);
+      break;
+    case 3:
+      nextPotentialMoveHorizontal.push(1, 2, 4, 5);
+      break;
+    case 4:
+      nextPotentialMoveHorizontal.push(2, 3, 5, 6);
+      break;
+    case 5:
+      nextPotentialMoveHorizontal.push(3, 4, 6, 7);
+      break;
+    case 6:
+      nextPotentialMoveHorizontal.push(4, 5, 7);
+      break;
+    case 7:
+      nextPotentialMoveHorizontal.push(5, 6);
+      break;
+  }
 
-//     chessboard[element[0]][element[1]] = 1
-    // knight.set(element);
-//   });
-//   console.log(knight.has([2, 1]))
-//   console.log(knight)
-//   console.log(chessboard)
-// }
+  switch (arr1[1]) {
+    case 0:
+      nextPotentialMoveVertical.push(1, 2);
+      break;
+    case 1:
+      nextPotentialMoveVertical.push(0, 1, 2);
+      break;
+    case 2:
+      nextPotentialMoveVertical.push(0, 1, 3, 4);
+      break;
+    case 3:
+      nextPotentialMoveVertical.push(1, 2, 4, 5);
+      break;
+    case 4:
+      nextPotentialMoveVertical.push(2, 3, 5, 6);
+      break;
+    case 5:
+      nextPotentialMoveVertical.push(3, 4, 6, 7);
+      break;
+    case 6:
+      nextPotentialMoveVertical.push(4, 5, 7);
+      break;
+    case 7:
+      nextPotentialMoveVertical.push(5, 6);
+      break;
+  }
 
-function lookForNextMoves(arr1){
-    let nextPotentialMoveHorizontal = []
-    let nextPotentialMoveVertical = []
-    let potentialMove = [];
-    switch(arr1[0]){
-        case 0: nextPotentialMoveHorizontal.push(1, 2); break
-        case 1: nextPotentialMoveHorizontal.push(0, 1, 2); break
-        case 2: nextPotentialMoveHorizontal.push(0, 1, 3, 4); break
-        case 3: nextPotentialMoveHorizontal.push(1, 2, 4, 5); break
-        case 4: nextPotentialMoveHorizontal.push(2, 3, 5, 6); break
-        case 5: nextPotentialMoveHorizontal.push(3, 4, 6, 7); break
-        case 6: nextPotentialMoveHorizontal.push(4, 5, 7); break
-        case 7: nextPotentialMoveHorizontal.push(5, 6); break
-      }
-    
-      switch(arr1[1]){
-        case 0: nextPotentialMoveVertical.push(1, 2); break
-        case 1: nextPotentialMoveVertical.push(0, 1, 2); break
-        case 2: nextPotentialMoveVertical.push(0, 1, 3, 4); break
-        case 3: nextPotentialMoveVertical.push(1, 2, 4, 5); break
-        case 4: nextPotentialMoveVertical.push(2, 3, 5, 6); break
-        case 5: nextPotentialMoveVertical.push(3, 4, 6, 7); break
-        case 6: nextPotentialMoveVertical.push(4, 5, 7); break
-        case 7: nextPotentialMoveVertical.push(5, 6); break
-      }
-    
-      for (let i = 0; i < nextPotentialMoveHorizontal.length; i++){
-        for (let j = 0; j < nextPotentialMoveVertical.length; j++){
-            if(((Math.abs(nextPotentialMoveHorizontal[i] - arr1[0]) % 3 === 1) && 
-            Math.abs((nextPotentialMoveVertical[j] - arr1[1]) % 3) === 2) || 
-            (Math.abs((nextPotentialMoveHorizontal[i] - arr1[0]) % 3) === 2 && 
-            Math.abs((nextPotentialMoveVertical[j] - arr1[1]) % 3) === 1)){
-                potentialMove.push([nextPotentialMoveHorizontal[i], nextPotentialMoveVertical[j]]);
-            }
+  for (let i = 0; i < nextPotentialMoveHorizontal.length; i++) {
+    for (let j = 0; j < nextPotentialMoveVertical.length; j++) {
+      if (
+        (Math.abs(nextPotentialMoveHorizontal[i] - arr1[0]) % 3 === 1 &&
+          Math.abs((nextPotentialMoveVertical[j] - arr1[1]) % 3) === 2) ||
+        (Math.abs((nextPotentialMoveHorizontal[i] - arr1[0]) % 3) === 2 &&
+          Math.abs((nextPotentialMoveVertical[j] - arr1[1]) % 3) === 1)
+      ) {
+        if (
+          !visitedQueue.has(
+            JSON.stringify([
+              nextPotentialMoveHorizontal[i],
+              nextPotentialMoveVertical[j],
+            ])
+          )
+        ) {
+          potentialMove.push([
+            nextPotentialMoveHorizontal[i],
+            nextPotentialMoveVertical[j],
+          ]);
         }
       }
-      return potentialMove
+    }
+  }
+  return potentialMove;
 }
-
-knightMoves([3,3], [4,3])
+// console.log(visitedQueue)
+knightMoves([3, 3], [4, 3]);
